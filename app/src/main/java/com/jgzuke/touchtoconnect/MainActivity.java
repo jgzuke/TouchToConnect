5f83f378c42f0419001af106ab5fd2f8f0de9fb2
jgzuke <jgzuke@gmail.com>
2015-06-30 22:53:04 -0400

    adding nfc stuff

--- app/src/main/java/com/jgzuke/touchtoconnect/MainActivity.java
+++ app/src/main/java/com/jgzuke/touchtoconnect/MainActivity.java
@@ -1,10 +1,18 @@
 package com.jgzuke.touchtoconnect;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.net.Uri;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcEvent;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Vibrator;
 import android.provider.ContactsContract;
 import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
@@ -22,14 +30,20 @@ import android.widget.Toast;
 import com.rey.material.widget.EditText;
 import com.rey.material.widget.FloatingActionButton;
 
+import java.nio.charset.Charset;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends Activity implements NfcAdapter.CreateNdefMessageCallback, NfcAdapter.OnNdefPushCompleteCallback {
 
     public static final String PREFERENCES = "touch-connect-shared-pref";
     public static final String PREF_NAME = "touch-connect-name";
     public static final String PREF_NUMBER = "touch-connect-number";
     public static final String PREF_EMAIL = "touch-connect-email";
     public static final String PREF_CONTACT = "touch-connect-contact";
+
+    public static final int BEAM_COMPLETE = 1;
+
+    public static final int VIBRATE_LENGTH = 1000;
 
     private EditText mNameInput;
     private EditText mNumberInput;
@@ -42,12 +56,15 @@ public class MainActivity extends Activity {
 
     private TextWatcher mTextWatcher;
 
+    private NfcAdapter mNfcAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mRes = getResources();
         mPref = getSharedPreferences(PREFERENCES, MODE_PRIVATE);
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
         mNameInput = (EditText) findViewById(R.id.name_input);
         mNumberInput = (EditText) findViewById(R.id.number_input);
@@ -92,6 +109,28 @@ public class MainActivity extends Activity {
         restoreLastText();
     }
 
+    @Override
+    public NdefMessage createNdefMessage(NfcEvent event) {
+        NdefRecord record = null;
+        return new NdefMessage(new NdefRecord[] {record});
+    }
+
+    @Override
+    public void onNdefPushComplete(NfcEvent event) {
+        mHandler.obtainMessage(BEAM_COMPLETE).sendToTarget();
+    }
+
+    private final Handler mHandler = new Handler()
+    {
+        @Override
+        public void handleMessage(Message message)
+        {
+            if(message.what == BEAM_COMPLETE) {
+                toast(R.string.beam_complete);
+            }
+        }
+    };
+
     private void restoreLastText() {
         if(mPref.contains(PREF_NAME)) {
             mNameInput.setText(mPref.getString(PREF_NAME, "Name"));
@@ -127,6 +166,9 @@ public class MainActivity extends Activity {
     private void setDataChanged() {
         mDoneFAB.setBackgroundColor(mRes.getColor(R.color.error_color));
     }
+
+
+    // ****UTILITY METHODS****
 
     private void setImageViewColor(int viewID, int color) {
         ((ImageView) findViewById(viewID)).setColorFilter(color);
